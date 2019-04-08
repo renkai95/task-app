46bdce221eed049424919ec8cf0922651dc8a6a3
rk <rk@rks-iMac.local>
2019-04-08 18:44:55 +1000

    Initial Commit

---
+++ assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment2
+//
+//  Created by rk on 8/4/19.
+//  Copyright © 2019 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
