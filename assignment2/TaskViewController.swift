de23348dc26fbf321a63a00c1b7e3880ffaf2b26
rk <rk@rks-iMac.local>
2019-04-09 20:43:30 +1000

    add files

--- assignment2/TaskViewController.swift
+++ assignment2/TaskViewController.swift
@@ -9,13 +9,48 @@
 import UIKit
 
 class TaskViewController: UIViewController {
+    @IBOutlet weak var descOutlet: UITextField!
 
+    @IBOutlet weak var dueOutlet: UITextField!
+    @IBOutlet weak var segOutlet: UISegmentedControl!
+    @IBOutlet weak var titleOutlet: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func createTask(_ sender: Any) {
+        if titleOutlet.text != "" && descOutlet.text != "" && dueOutlet.text!==""
+        {
+            let title = titleOutlet.text!
+            let desc = descOutlet.text!
+            let due=dueOutlet.text!
+            let newTask = Task(name: title, description: desc,duedate:due,)
+            
+            let _ = superHeroDelegate!.addSuperHero(newHero: hero)
+            navigationController?.popViewController(animated: true)
+            return
+        }
+        
+        var errorMsg = "Please ensure all fields are filled:\n"
+        
+        if nameTextField.text == "" {
+            errorMsg += "- Must provide a name\n"
+        }
+        if abilitiesTextField.text == "" {
+            errorMsg += "- Must provide abilities"
+        }
+        
+        displayMessage(title: "Not all fields filled", message: errorMsg)
+    }
     
+    func displayMessage(title: String, message: String) {
+        // Setup an alert to show user details about the Person
+        // UIAlertController manages an alert instance
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default,handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
--- assignment2/ViewController.swift
+++ assignment2/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
     }
+    
 
 
 }
